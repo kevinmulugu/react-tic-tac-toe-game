18be7e1d361471518efc21f85b84244641980b33
Kevin Mulugu <kevinmulugu@gmail.com>
2024-04-03 15:40:29 +0300

    upate: used react's useState to hold X's for Square clicks

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
+import { useState } from "react";
+
 export default function Board() {
     return <>
         <div className="board-row">
-            <div className="square">X</div>
-            <div className="square">X</div>
-            <div className="square">x</div>
+            <Square />
+            <Square />
+            <Square />
         </div>
         <div className="board-row">
-            <div className="square">X</div>
-            <div className="square">X</div>
-            <div className="square">X</div>
+            <Square />
+            <Square />
+            <Square />
         </div>
         <div className="board-row">
-            <div className="square">X</div>
-            <div className="square">X</div>
-            <div className="square">X</div>
+            <Square />
+            <Square />
+            <Square />
         </div>
     </>;
+}
+
+function Square() {
+    const [value, setValue] = useState(null);
+
+    function handleClick() {
+        setValue("X");
+    }
+    return <button className="square" onClick={handleClick}>{value}</button>;
 }
