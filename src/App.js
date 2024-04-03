c73d6cc85d43f601e1c83c7bf29a4a1c8e539924
Kevin Mulugu <kevinmulugu@gmail.com>
2024-04-03 14:59:07 +0300

    update: initialization with a board

---
+++ src/App.js
@@ -0,0 +1,19 @@
+export default function Board() {
+    return <>
+        <div className="board-row">
+            <div className="square">X</div>
+            <div className="square">X</div>
+            <div className="square">x</div>
+        </div>
+        <div className="board-row">
+            <div className="square">X</div>
+            <div className="square">X</div>
+            <div className="square">X</div>
+        </div>
+        <div className="board-row">
+            <div className="square">X</div>
+            <div className="square">X</div>
+            <div className="square">X</div>
+        </div>
+    </>;
+}
